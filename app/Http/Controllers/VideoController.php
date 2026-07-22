1a817c50c18b5523ef803f18a6b74389fdc8ede4
therdil <erdil_02@hotmail.com>
2026-07-22 14:26:40 +0300

    TurTube v0.9.2 - Complete video management system

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -35,7 +35,8 @@ class VideoController extends Controller
             'duration' => 0,
         ]);
 
-        return redirect('/')
+        return redirect()
+            ->route('videos.mine')
             ->with('success', 'Video başarıyla yüklendi!');
     }
 
